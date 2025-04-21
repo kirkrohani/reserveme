d80cfc31f3af83369bfffe1d78496006a4e1b20f
kirkrohani <kirk.rohani@gmail.com>
2025-04-21 13:36:34 -0500

    added decorators to create-reservation dto to validate incoming data set whitelist to true to strip extraneous data sent

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,19 @@
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDateString()
+  @IsNotEmpty()
   startDate: Date;
+
+  @IsDateString()
+  @IsNotEmpty()
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
