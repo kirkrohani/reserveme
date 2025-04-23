5666387340a9313be16ef72d71a7ddf1e3fe2fdf
kirkrohani <kirk.rohani@gmail.com>
2025-04-22 21:36:41 -0500

    separated out into multiple env files, per app updated docker-compose to load separate env files updated auth module to use jwtModule from nestjs updated auth module to perform validation for env vars updated main.ts for auth and reservations to pull PORT from env file updated reservations module to validate reservations env file removed config module altogether

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3001);
+  const configService: ConfigService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+  await app.listen(process.env.port ?? configService.get('PORT'));
+  console.log(`***** Auth App listening on port ${PORT}`);
 }
 bootstrap();
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3000);
+  const configService: ConfigService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+  await app.listen(process.env.port ?? PORT);
+  console.log(`***** Reservations App listening on port ${PORT}`);
 }
 bootstrap();
