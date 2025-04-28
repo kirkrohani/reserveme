f73a057376594ca872489911d41a117ced0e8a39
kirkrohani <kirk.rohani@gmail.com>
2025-04-28 16:35:25 -0500

    installed stripe npm package added Logger to payments main.ts added stripe secret key to payments module initialized stripe client in payments service

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
+  private readonly stripeClient = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2025-03-31.basil',
+    },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
