8a072a0b2cb7425a79cf48634da75a7712c93ca8
kirkrohani <kirk.rohani@gmail.com>
2025-04-29 20:30:47 -0500

    created notify-email dto created payments-create-charge dto created notifyEmail methods in notifications controller and service

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { NotifyEmailDto } from './dto/notify-email.dto';
+import { ReservationsController } from 'apps/reservations/src/reservations.controller';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  protected readonly logger: Logger = new Logger(ReservationsController.name);
+
+  async notifyEmail({ email }: NotifyEmailDto) {
+    this.logger.log('\n----------> Notifications Service notifyEmail()', email);
+  }
+}
