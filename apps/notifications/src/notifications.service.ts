5ec6eb6e84dd0b8f296e35cf949ef27a51833059
kirkrohani <kirk.rohani@gmail.com>
2025-04-30 22:05:26 -0500

    updated notifications service and added an smtp transport using nodemailer using gmail as the service provider

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,11 +1,32 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotificationsService {
   protected readonly logger: Logger = new Logger(NotificationsService.name);
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+    },
+  });
 
   async notifyEmail({ email }: NotifyEmailDto) {
-    this.logger.log(`\n---------->  notifyEmail() ${email}`);
+    this.logger.log(`\n------------------------>  notifyEmail() ${email}`);
+
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'ReserveMe Notification',
+      text: 'Hello World, from ReserveMe App',
+    });
   }
 }
