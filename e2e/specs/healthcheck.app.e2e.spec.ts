749df19a2c428f84ce931bd4074b7f9754e84031
kirkrohani <kirk.rohani@gmail.com>
2025-05-24 18:31:29 -0500

    updated api endpoint tests

--- e2e/specs/healthcheck.app.e2e.spec.ts
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -1,13 +1,13 @@
-describe('Health', () => {
+describe('RESERVATIONS HEALTH', () => {
   test('Reservations response OK', async () => {
     const response = await fetch('http://reservations:3000');
     expect(response.ok).toBeTruthy();
   });
 });
 
-describe('Health', () => {
+describe('AUTH HEALTH', () => {
   test('Auth response OK', async () => {
-    const response = await fetch('http://auth:3003');
+    const response = await fetch('http://auth:3001');
     expect(response.ok).toBeTruthy();
   });
 });
